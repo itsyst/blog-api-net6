e46f15a7f6092afdd123b4cc74877657fd368470
Khaled El Hamzi <contact@elhamzi.me>
2022-02-13 12:59:40 +0100

    Add project files.

---
+++ Blog.Domain/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog.Domain
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Post> Posts { get; set; }
+    }
+}
---
+++ Blog.Domain/Post.cs
@@ -0,0 +1,12 @@
+﻿namespace Blog.Domain
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string Content { get; set; }
+        public Category Category { get; set; }
+        public Guid CategoryId { get; set; }
+    }
+}
