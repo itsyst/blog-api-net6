3b34071881dbc99ee508cceca6fff1fe810f8939
Khaled El Hamzi <contact@elhamzi.me>
2022-02-13 23:54:29 +0100

    Fix generic interface.

--- Blog.Application/Interfaces/IAsyncRepository.cs
+++ Blog.Application/Interfaces/IAsyncRepository.cs
@@ -3,7 +3,7 @@
     public interface IAsyncRepository<T> where T : class
     {
         Task<T> GetByIdAsync(Guid id);
-        Task<IReadOnlyList<T>> GetAllAsync(Guid id);
+        Task<IReadOnlyList<T>> GetAllAsync();
         Task<T> AddAsync(T entity);
         Task<T> UpdateAsync(T entity);
         Task<T> DeleteAsync(T entity);
