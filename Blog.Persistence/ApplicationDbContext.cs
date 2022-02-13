195dfaec7bd64e609dbdecf5797f31f49eb0ffbc
Khaled El Hamzi <contact@elhamzi.me>
2022-02-13 23:56:45 +0100

    Add application dbcontext.

---
+++ Blog.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Persistence
+{
+#pragma warning disable
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
