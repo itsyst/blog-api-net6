31ea87b23853d5353bdedb1099259a753e77b328
Khaled El Hamzi <contact@elhamzi.me>
2022-02-13 23:53:26 +0100

    Add delete post command.

---
+++ Blog.Application/Features/Posts/Commands/Delete/DeletePostCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Blog.Application.Features.Posts.Commands.Delete
+{
+    public class DeletePostCommand : IRequest
+    {
+        public Guid PostId { get; set; }
+    }
+}
---
+++ Blog.Application/Features/Posts/Commands/Delete/DeletePostCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using Blog.Application.Interfaces;
+using MediatR;
+
+namespace Blog.Application.Features.Posts.Commands.Delete
+{
+    public class DeletePostCommandHandler : IRequestHandler<DeletePostCommand>
+    {
+        private readonly IPostRepository _postRepository;
+        public DeletePostCommandHandler(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+        public async Task<Unit> Handle(DeletePostCommand request, CancellationToken cancellationToken)
+        {
+            var post = await _postRepository.GetPostByIdAsync(request.PostId);  
+            await _postRepository.DeleteAsync(post);
+            return Unit.Value;
+        }
+    }
+}
