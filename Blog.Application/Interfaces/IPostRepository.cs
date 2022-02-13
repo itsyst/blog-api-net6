4c2cfedde1620c742155bedfc01dd8bb121680f9
Khaled El Hamzi <contact@elhamzi.me>
2022-02-13 15:51:22 +0100

    Create post repository interface.

---
+++ Blog.Application/Interfaces/IPostRepository.cs
@@ -0,0 +1,11 @@
+﻿using Blog.Domain;
+using System.Linq.Expressions;
+
+namespace Blog.Application.Interfaces
+{
+    public interface IPostRepository : IAsyncRepository<Post>
+    {
+        Task<Post> GetPostByIdAsync(Guid id, Expression<Func<Post, bool>>? filter = null, string? includeProperties = null, bool tracked = true);
+        Task<IReadOnlyList<Post>> GetAllPostsAsync(Expression<Func<Post, bool>>? filter = null, params Expression<Func<Post, object>>[] includeProperties);
+    }
+}
