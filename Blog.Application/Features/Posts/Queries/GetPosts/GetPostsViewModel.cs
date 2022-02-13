dc13dc146271513b8c603518a66dd4d84751544a
Khaled El Hamzi <contact@elhamzi.me>
2022-02-13 16:17:16 +0100

    Applicate CQRS pattern.

---
+++ Blog.Application/Features/Posts/Queries/GetPosts/GetPostsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Application.Features.Posts.Queries.GetPosts
+{
+    public class GetPostsViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        public string? ImageUrl { get; set; }
+
+        [Required]
+        public string Excerpt { get; set; } = string.Empty;
+    }
+}
