c987f4a1f60ee1aac05de673fea0cc148a9b9bc8
Khaled El Hamzi <contact@elhamzi.me>
2022-02-13 15:51:56 +0100

    Update category model.

--- Blog.Domain/Category.cs
+++ Blog.Domain/Category.cs
@@ -1,9 +1,12 @@
-﻿namespace Blog.Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Domain
 {
     public class Category
     {
+        [Key]
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public ICollection<Post> Posts { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<Post> Posts { get; set; } = new List<Post>();    
     }
 }
