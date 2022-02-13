47191f2967a87fa123e57f5d59e2f127a8887468
Khaled El Hamzi <contact@elhamzi.me>
2022-02-13 23:57:11 +0100

    Create base repository.

---
+++ Blog.Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,47 @@
+﻿using Blog.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Persistence.Repositories
+{
+#pragma warning disable
+
+    public partial class BaseRepository<T>: IAsyncRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+        public virtual async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await  _context.SaveChangesAsync();
+
+            return entity;
+        }
+        public async Task<T> DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+    }
+}
