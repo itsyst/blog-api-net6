73b7e5be0161f8e619b8c81b3b97cb8a0deac559
Khaled El Hamzi <contact@elhamzi.me>
2022-02-13 15:52:25 +0100

    Add properties to post model.

--- Blog.Domain/Post.cs
+++ Blog.Domain/Post.cs
@@ -1,12 +1,33 @@
-﻿namespace Blog.Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Domain
 {
     public class Post
     {
+        [Key]
         public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string ImageUrl { get; set; }
-        public string Content { get; set; }
-        public Category Category { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        public string? ImageUrl { get; set; }
+
+        [Required]
+        public string Excerpt { get; set; } = string.Empty;
+
+        [Required]
+        public string Content { get; set; } = string.Empty;
+ 
+
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+
+        public Category Category { get; set; } = new Category();
         public Guid CategoryId { get; set; }
+
+
+        public ICollection<Tag> Tags { get; } = new List<Tag>();
+
+        public ICollection<Comment> Comments { get; } = new List<Comment>();
     }
 }
