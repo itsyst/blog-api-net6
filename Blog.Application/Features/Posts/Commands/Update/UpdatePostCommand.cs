e0e7ad4bec3ad367d7812bb7aeb7edce5e26ef2c
Khaled El Hamzi <contact@elhamzi.me>
2022-02-13 23:53:46 +0100

    Add update post command.

---
+++ Blog.Application/Features/Posts/Commands/Update/UpdatePostCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace Blog.Application.Features.Posts.Commands.Update
+{
+    public class UpdatePostCommand : IRequest
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? ImageUrl { get; set; }
+
+        public string Excerpt { get; set; } = string.Empty;
+
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public Guid CategoryId { get; set; }
+    }
+}
---
+++ Blog.Application/Features/Posts/Commands/Update/UpdatePostCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Blog.Application.Interfaces;
+using Blog.Domain;
+using MediatR;
+
+namespace Blog.Application.Features.Posts.Commands.Update
+{
+    public class UpdatePostCommandHandler : IRequestHandler<UpdatePostCommand>
+    {
+        private readonly IPostRepository _postRepository;
+        private readonly IMapper _mapper;
+
+        public UpdatePostCommandHandler(IPostRepository postRepository, IMapper mapper)
+        {
+            _postRepository = postRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(UpdatePostCommand request, CancellationToken cancellationToken)
+        {
+            Post post = _mapper.Map<Post>(request);
+            await _postRepository.UpdateAsync(post);
+            return Unit.Value;
+        }
+    }
+}
