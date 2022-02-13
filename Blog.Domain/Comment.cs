fa2191842341c3c3c6aa5435b6d3d38235167121
Khaled El Hamzi <contact@elhamzi.me>
2022-02-13 15:53:08 +0100

    Create comments and tags models.

---
+++ Blog.Domain/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Domain
+{
+    public class Comment
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Content { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
---
+++ Blog.Domain/Tag.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Domain
+{
+    public class Tag
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
