a77bd3993bd126eebf942578bc35c83fc58e1c59
Khaled El Hamzi <contact@elhamzi.me>
2022-02-13 14:58:11 +0100

    Create a generic async repository.

---
+++ Blog.Application/Interfaces/IAsyncRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog.Application.Interfaces
+{
+    public interface IAsyncRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<T>> GetAllAsync(Guid id);
+        Task<T> AddAsync(T entity);
+        Task<T> Update(T entity);
+        Task<T> DeleteAsync(T entity);
+    }
+}
