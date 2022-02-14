ecb48baa1046b8e8651715137479985834587c19
Khaled El Hamzi <contact@elhamzi.me>
2022-02-14 15:30:41 +0100

    Create application and persistence container.

---
+++ Blog.Application/ApplicationContainer.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Blog.Application
+{
+    public static class ApplicationContainer
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
---
+++ Blog.Persistence/PersistenceContainer.cs
@@ -0,0 +1,24 @@
+﻿using Blog.Application.Interfaces;
+using Blog.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blog.Persistence
+{
+    public static class PersistenceContainer
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(
+                options =>options.UseSqlServer(
+                    configuration.GetConnectionString("BlogConnectionString"), 
+                    providerOptions => providerOptions.EnableRetryOnFailure()));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+            services.AddScoped(typeof(IPostRepository), typeof(PostRepository));
+
+            return services;
+        }
+    }
+}
