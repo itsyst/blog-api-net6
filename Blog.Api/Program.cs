f11116f7bd94a4eefd0686d05dc58355d63ff24e
Khaled El Hamzi <contact@elhamzi.me>
2022-02-14 15:34:22 +0100

    Add services and invoke the database method at runtime.

--- Blog.Api/Program.cs
+++ Blog.Api/Program.cs
@@ -1,20 +1,41 @@
+using Blog.Application;
+using Blog.Application.Interfaces;
+using Blog.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddPersistenceServices(builder.Configuration);
+builder.Services.AddApplicationServices();
 builder.Services.AddControllers();
+
+// Database initializer
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApiVersioning();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(options =>
+    {
+        options.SerializeAsV2 = true;
+    });
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Blog API V1");
+        options.RoutePrefix = "swagger";
+    });
 }
+
+//Invoke function to seed database
+SeedDatabase();
 
 app.UseHttpsRedirection();
 
@@ -23,3 +44,11 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+// Seed Database
+void SeedDatabase()
+{
+    using var scope = app.Services.CreateAsyncScope();
+    var initializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+    initializer.Initialize();
+}
