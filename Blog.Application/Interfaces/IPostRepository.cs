c2a30ef4eceab10d85e90ea7f1c26fd4e134590a
Khaled El Hamzi <contact@elhamzi.me>
2022-02-14 15:27:30 +0100

    Fix eager loading in contract member.

--- Blog.Application/Interfaces/IPostRepository.cs
+++ Blog.Application/Interfaces/IPostRepository.cs
@@ -5,7 +5,7 @@ namespace Blog.Application.Interfaces
 {
     public interface IPostRepository : IAsyncRepository<Post>
     {
-        Task<Post> GetPostByIdAsync(Guid id, string? includeProperties = null, bool tracked = true);
+        Task<Post> GetPostByIdAsync(Guid id, bool tracked = true, params Expression<Func<Post, object>>[] includeProperties);
         Task<IReadOnlyList<Post>> GetAllPostsAsync(Expression<Func<Post, bool>>? filter = null, params Expression<Func<Post, object>>[] includeProperties);
     }
 }
